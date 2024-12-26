5b1dba5a5d58e85ef2a4931d4fb2401b34ae171f
= <=>
2024-12-25 21:33:07 -0300

    add allure as report

--- tests/main.py
+++ tests/main.py
@@ -1,28 +1,53 @@
+import pytest
 from config import get_driver
 from automation import SauceDemoAutomation
 from config import SAUCEDO_USERNAME, SAUCEDO_PASSWORD, FIRST_NAME, LAST_NAME, POSTAL_CODE
+import allure
 
-def main():
+
+@pytest.fixture
+def driver():
+    """Fixture to initialize and quit the WebDriver."""
     driver = get_driver()
+    yield driver
+    driver.quit()
+
+
+@allure.title("SauceDemo Full Purchase Flow")
+def test_sauce_demo_purchase_flow(driver):
+    """Test case for the full purchase flow in SauceDemo."""
     automation = SauceDemoAutomation(driver)
-    
-    try:
+
+    with allure.step("Open SauceDemo website"):
         automation.open_website("https://www.saucedemo.com/")
+
+    with allure.step("Log in to SauceDemo"):
         automation.login(SAUCEDO_USERNAME, SAUCEDO_PASSWORD)
+
+    with allure.step("Add items to cart"):
         automation.add_items_to_cart([
             "add-to-cart-sauce-labs-backpack",
             "add-to-cart-sauce-labs-bike-light",
             "add-to-cart-sauce-labs-fleece-jacket"
         ])
-        automation.go_to_cart()
-        automation.remove_item_from_cart("remove-sauce-labs-backpack")
-        automation.checkout(FIRST_NAME, LAST_NAME, POSTAL_CODE)
-        automation.save_screenshot("screenshot1.png")
-        automation.finish_purchase()
-        automation.validate_button_styles()
-        automation.logout()
-    finally:
-        automation.quit()
 
-if __name__ == "__main__":
-    main()
+    with allure.step("Go to the cart"):
+        automation.go_to_cart()
+
+    with allure.step("Remove an item from the cart"):
+        automation.remove_item_from_cart("remove-sauce-labs-backpack")
+
+    with allure.step("Checkout items"):
+        automation.checkout(FIRST_NAME, LAST_NAME, POSTAL_CODE)
+
+    with allure.step("Take a screenshot after checkout"):
+        automation.save_screenshot("screenshot1.png")
+
+    with allure.step("Finish the purchase process"):
+        automation.finish_purchase()
+
+    with allure.step("Validate button styles on the confirmation page"):
+        automation.validate_button_styles()
+
+    with allure.step("Log out of SauceDemo"):
+        automation.logout()
